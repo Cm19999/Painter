ab2b61f28e31281f64e833e49b8057e03ba61967
callum <cmarsden4163@googlemail.com>
2021-10-12 15:39:48 +0100

    added enhanced switch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 import java.lang.Math;
 
@@ -13,30 +14,31 @@ public class Main {
             System.out.println(paints);
         }
         String paintName = myObj.nextLine();
-        
+
         System.out.println("Do you know how many tins you need?(Y or N)");
         String answer = myObj.nextLine();
 
-        if (answer.equals("Y") ){
-            System.out.println("How many tins of paint do you need?");
-            int litres = Integer.parseInt(myObj.nextLine());
-            costCalculatorTins(litres,paintName);
-        }
-        else {
-            System.out.println("What area do you need to be painted?");
-            double area = Double.parseDouble(myObj.nextLine());
-            costCalculatorArea(area,paintName);
+        switch (answer) {
+            case "Y" -> costCalculatorTins(paintName);
+            case "N" -> costCalculatorArea(paintName);
         }
 
     }
-    public static void costCalculatorTins(int litres,String paintName){
+    public static void costCalculatorTins(String paintName){
+        Scanner myObj2 = new Scanner(System.in);
+        System.out.println("How many tins of paint do you need?");
+        int litres = Integer.parseInt(myObj2.nextLine());
 
         Paint paint = Paint.valueOf(paintName);
         double paintCost = paint.getPaintCost();
         System.out.println("The cost will be £" + paintCost*litres);
 
     }
-    public static void costCalculatorArea(double area, String paintName){
+    public static void costCalculatorArea(String paintName){
+        Scanner myObj3 = new Scanner(System.in);
+        System.out.println("What area do you need to be painted?");
+        double area = Double.parseDouble(myObj3.nextLine());
+
         double areaPerLitre = 2.5;
         Paint paint = Paint.valueOf(paintName);
         double paintCost = paint.getPaintCost();
