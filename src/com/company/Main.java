345b7473cdb6667e8d55e17067d85bc0e507972c
callum <cmarsden4163@googlemail.com>
2021-10-12 10:51:56 +0100

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,12 @@
 package com.company;
 
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Main {
     public static void main(String[] args) {
+
+
 
         Scanner myObj = new Scanner(System.in);
         System.out.println("What type of paint do you want? Out of: ");
@@ -12,17 +15,39 @@ public class Main {
         }
         String paintName = myObj.nextLine();
 
-        Scanner myObj2 = new Scanner(System.in);
-        System.out.println("How many tins of paint do you need?");
-        int litres = Integer.parseInt(myObj2.nextLine());
+        Scanner myObj3 = new Scanner(System.in);
+        System.out.println("Do you know how many tins you need?(Y or N)");
+        String answer = myObj3.nextLine();
+        System.out.println(answer);
 
-        costCalculator(litres,paintName);
+        if (answer.equals("Y") ){
+            Scanner myObj2 = new Scanner(System.in);
+            System.out.println("How many tins of paint do you need?");
+            int litres = Integer.parseInt(myObj2.nextLine());
+            costCalculatorTins(litres,paintName);
+        }
+        else {
+            Scanner myObj4 = new Scanner(System.in);
+            System.out.println("What area do you need to be painted?");
+            double area = Double.parseDouble(myObj4.nextLine());
+            costCalculatorArea(area,paintName);
+        }
+
+
     }
-    public static void costCalculator(int litres,String paintName){
+    public static void costCalculatorTins(int litres,String paintName){
 
         Paint paint = Paint.valueOf(paintName);
         double paintCost = paint.getPaintCost();
         System.out.println("The cost will be £" + paintCost*litres);
+
+    }
+    public static void costCalculatorArea(double area, String paintName){
+        double areaPerLitre = 2.5;
+        Paint paint = Paint.valueOf(paintName);
+        double paintCost = paint.getPaintCost();
+        int amountOfTins = (int) Math.ceil(area/areaPerLitre);
+        System.out.println("The cost will be £" + paintCost*amountOfTins);
 
     }
 }
